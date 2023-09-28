4c5524e0403aea3203b96d08343ea4f799ec946f
Dmitriy <dima.komlyk@gmail.com>
2023-09-28 09:43:43 +0300

    fix: fix env production vercel build

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   env: {
     NEXT_PUBLIC_SERVER_API_KEY: process.env.NEXT_PUBLIC_SERVER_API_KEY,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
   images: {
     domains: ['res.cloudinary.com'],
